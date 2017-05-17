0d5a5306b8eef525906e1c4b0ba73015d5f6f987
yolazuka <yola.martin@gmail.com>
2017-05-17 22:13:28 +0200

    Update 17/05/2017

--- app/src/main/java/com/example/usuario/music4emotions/happy_artist_two.java
+++ app/src/main/java/com/example/usuario/music4emotions/happy_artist_two.java
@@ -1,7 +1,11 @@
 package com.example.usuario.music4emotions;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class happy_artist_two extends AppCompatActivity {
 
@@ -9,5 +13,38 @@ public class happy_artist_two extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_happy_artist_two);
+
+        // Find the View that shows the text to go to the MAIN screen
+        TextView GoToMain = (TextView) findViewById(R.id.menu_main_artist_profile);
+
+        // Set a click listener on that View
+        GoToMain.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the text Music Store is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent goToMainIntent = new Intent(happy_artist_two.this, MainActivity.class);
+                startActivity(goToMainIntent);
+            }
+        });
+
+        // Find the View that shows the text to go to the Music Store screen
+        TextView MusicStore = (TextView) findViewById(R.id.music_store);
+
+        // Set a click listener on that View
+        MusicStore.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the text Music Store is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent goToStoreIntent = new Intent(happy_artist_two.this, Store.class);
+                startActivity(goToStoreIntent);
+            }
+        });
+    }
+
+    public void GoToSearch(View view) {
+        TextView search = (TextView) findViewById(R.id.search);
+        Toast.makeText(this, "This action would open a fragment with a searching field", Toast.LENGTH_SHORT).show();
+        ;
+
     }
 }
